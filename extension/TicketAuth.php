98b879c989f5b4d4589355dada3d8b99d9864850
Lars Vierbergen <vierbergenlars@gmail.com>
2015-06-18 20:19:10 +0200

    Tabs should be spaces

--- extension/TicketAuth.php
+++ extension/TicketAuth.php
@@ -38,12 +38,12 @@ http://mywiki.com/w/index.php/Main_Page
 
 
 $wgExtensionCredits['other'][] = array (
-	'name' => 'Ticket Authentication',
-	'author' => 'Iaroslav Vassiliev <codedriller@gmail.com>',
-	'description' => 'Authenticates users using simple web ticket validation mechanism',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:Ticket_Authentication',
-	'version' => 1.11,
-	'path' => __FILE__
+    'name' => 'Ticket Authentication',
+    'author' => 'Iaroslav Vassiliev <codedriller@gmail.com>',
+    'description' => 'Authenticates users using simple web ticket validation mechanism',
+    'url' => 'https://www.mediawiki.org/wiki/Extension:Ticket_Authentication',
+    'version' => 1.11,
+    'path' => __FILE__
 );
 
 $wgHooks['UserLoadFromSession'][] = 'efTktAuth_OnUserLoadFromSession';
@@ -52,10 +52,10 @@ $wgHooks['SpecialPasswordResetOnSubmit'][] = 'efTktAuth_OnSpecialPasswordResetOn
 $wgHooks['UserLoginMailPassword'][] = 'efTktAuth_OnUserLoginMailPassword';
 
 function efTktAuth_OnUserLoadFromSession ( $user, &$result )
-	// $user: user object being loaded
-	// &$result: set this to a boolean value to abort the normal authentication process
+    // $user: user object being loaded
+    // &$result: set this to a boolean value to abort the normal authentication process
 {
-	global $wgRequest, $wgUser, $wgContLang, $wgOut, $wgTktAuth_BridgeURL;
+    global $wgRequest, $wgUser, $wgContLang, $wgOut, $wgTktAuth_BridgeURL;
 
     $page = Title::newFromText($wgRequest->getVal('title'));
     if($page&&$page->isSpecial('Userlogin')) {
@@ -63,209 +63,210 @@ function efTktAuth_OnUserLoadFromSession ( $user, &$result )
         exit;
     }
 
-	$username = $wgRequest->getVal( 'user' );
-	if ( $username == null || $username == '' )
-		return false;
+    $username = $wgRequest->getVal( 'user' );
+    if ( $username == null || $username == '' )
+        return false;
 
-	$signature = $wgRequest->getVal( 'sign', '' );
-	$timestamp = $wgRequest->getInt( 'time', 0 );
-	if ( $signature == '' || $timestamp == 0 )
-		return false;
+    $signature = $wgRequest->getVal( 'sign', '' );
+    $timestamp = $wgRequest->getInt( 'time', 0 );
+    if ( $signature == '' || $timestamp == 0 )
+        return false;
 
-	$realName = $wgRequest->getVal( 'name', '' );
-	$email = $wgRequest->getVal( 'email', '' );
-	$password = $wgRequest->getVal( 'password', '' );
+    $realName = $wgRequest->getVal( 'name', '' );
+    $email = $wgRequest->getVal( 'email', '' );
+    $password = $wgRequest->getVal( 'password', '' );
 
 
-	if ( !isset( $GLOBALS['wgTktAuth_SecretKey'] ) )
-		$error = 'TicketAuth: wgTktAuth_SecretKey required global variable is not set.';
-	if ( !isset( $GLOBALS['wgTktAuth_TicketExpiryMinutes'] ) )
-		$error = 'TicketAuth: wgTktAuth_TicketExpiryMinutes required global variable is not set.';
-	if ( isset( $error ) ) {
-		wfDebug( $error . "\n" );
-		return false;
-	}
+    if ( !isset( $GLOBALS['wgTktAuth_SecretKey'] ) )
+        $error = 'TicketAuth: wgTktAuth_SecretKey required global variable is not set.';
+    if ( !isset( $GLOBALS['wgTktAuth_TicketExpiryMinutes'] ) )
+        $error = 'TicketAuth: wgTktAuth_TicketExpiryMinutes required global variable is not set.';
+    if ( isset( $error ) ) {
+        wfDebug( $error . "\n" );
+        return false;
+    }
 
-	if ( $email != '' && !Sanitizer::validateEmail( $email ) ) {
-		$error = 'TicketAuth: Invalid e-mail address was provided.';
-		wfDebug( $error . "\n" );
-		return false;
-	}
+    if ( $email != '' && !Sanitizer::validateEmail( $email ) ) {
+        $error = 'TicketAuth: Invalid e-mail address was provided.';
+        wfDebug( $error . "\n" );
+        return false;
+    }
 
-	if ( $timestamp + ((int) $GLOBALS['wgTktAuth_TicketExpiryMinutes']) * 60 < time() ) {
-		$error = 'TicketAuth: Provided ticket has expired. ';
-		wfDebug( $error . "\n" );
-		return false;
-	}
+    if ( $timestamp + ((int) $GLOBALS['wgTktAuth_TicketExpiryMinutes']) * 60 < time() ) {
+        $error = 'TicketAuth: Provided ticket has expired. ';
+        wfDebug( $error . "\n" );
+        return false;
+    }
 
-	if ( $password != '' && !preg_match( '/^[0-9a-f]{32}$/i', $password ) ) {
-		$error = 'TicketAuth: Invalid password MD5 hash was provided.';
-		wfDebug( $error . "\n" );
-		return false;
-	}
-	if ( $password != '' && isset( $wgPasswordSalt ) ) {
-		$error = 'TicketAuth: Transferring password hash is incompatible with ' .
-			'$wgPasswordSalt setting.';
-		wfDebug( $error . "\n" );
-		return false;
-	}
-	
-	$data = 'user=' . urlencode( $username ) .
-	( $password? '&password=' . $password : '' ) .
-	( $realName ? '&name=' . urlencode( $realName ) : '' ) .
-	( $email ? '&email=' . urlencode( $email ) : '' ) .
-	'&time=' . $timestamp;
+    if ( $password != '' && !preg_match( '/^[0-9a-f]{32}$/i', $password ) ) {
+        $error = 'TicketAuth: Invalid password MD5 hash was provided.';
+        wfDebug( $error . "\n" );
+        return false;
+    }
+    if ( $password != '' && isset( $wgPasswordSalt ) ) {
+        $error = 'TicketAuth: Transferring password hash is incompatible with ' .
+            '$wgPasswordSalt setting.';
+        wfDebug( $error . "\n" );
+        return false;
+    }
+    
+    $data = 'user=' . urlencode( $username ) .
+    ( $password? '&password=' . $password : '' ) .
+    ( $realName ? '&name=' . urlencode( $realName ) : '' ) .
+    ( $email ? '&email=' . urlencode( $email ) : '' ) .
+    '&time=' . $timestamp;
 
-	$validSignature = hash_hmac("sha256", $data, $GLOBALS['wgTktAuth_SecretKey']);
+    $validSignature = hash_hmac("sha256", $data, $GLOBALS['wgTktAuth_SecretKey']);
 
-	if ( $signature != $validSignature ) {
-		$error = 'TicketAuth: Ticket signature is invalid.';
-		wfDebug( $error . "\n" );
-		return false;
-	}
+    if ( $signature != $validSignature ) {
+        $error = 'TicketAuth: Ticket signature is invalid.';
+        wfDebug( $error . "\n" );
+        return false;
+    }
 
-	$username = User::getCanonicalName( $username, 'creatable' );
-	if ( $username === false ) {
-		$error = 'TicketAuth: Provided username is not valid for MediaWiki.';
-		wfDebug( $error . "\n" );
-		return false;
-	}
+    $username = User::getCanonicalName( $username, 'creatable' );
+    if ( $username === false ) {
+        $error = 'TicketAuth: Provided username is not valid for MediaWiki.';
+        wfDebug( $error . "\n" );
+        return false;
+    }
 
-	$user = User::newFromName( $username );
-	if ( !$user->getID() ) {
-		wfDebug( "TicketAuth: Creating new user account.\n" );
-		$userInfo = array();
-		if ( $realName != '' ) {
-			$userInfo['real_name'] = $realName;
-		}
-		if ( $email != '' ) {
-			$userInfo['email'] = $email;
-			$userInfo['email_authenticated'] = wfTimestampNow();
-		}
-		if ( $password != '' ) {
-			$userInfo['password'] = $password;    // incompatible with $wgPasswordSalt
-		}
-		$user = User::createNew( $username, $userInfo );
-		if ( !$user ) {
-			$error = 'TicketAuth: Could not create user.';
-			wfDebug( $error . "\n" );
-			return false;
-		}
-		$user->setOption( 'TicketAuth', true );
-		$user->saveSettings();
-		if ( session_id() == '' ) {
-			wfSetupSession();
-		}
-		$user->setCookies();
-		$wgUser = $user;
-		$wgOut->getContext()->setUser( $user );
-		wfRunHooks( 'AddNewAccount', array( $user, false ) );
-		$user->addNewUserLogEntry( 'autocreate', 'TicketAuth' );
-	} else {
-		wfDebug( "TicketAuth: Logging user in.\n" );
-		if ( !$user->getBoolOption( 'TicketAuth' ) ) {
-			$error = 'TicketAuth: User\'s account with this login name already exists and ' .
-				'it was not created by TicketAuth. Can\'t authenticate.';
-			wfDebug( $error . "\n" );
-			return false;
-		}
-		if ( $realName != '' ) {
-			$user->setRealName( $realName );
-		}
-		if ( $email != '' ) {
-			$user->setEmail( $email );
-			$user->mEmailAuthenticated = wfTimestampNow();
-		}
-		if ( $password != '' ) {
-			$user->mPassword = $password;    // incompatible with $wgPasswordSalt
-		}
-		$user->saveSettings();
-		if ( session_id() == '' ) {
-			wfSetupSession();
-		}
-		$user->setCookies();
-		$wgUser = $user;
-		$wgOut->getContext()->setUser( $user );
-	}
+    $user = User::newFromName( $username );
 
-	header('Location: '.explode('?',$wgRequest->getRequestURL())[0]);
+    if ( !$user->getID() ) {
+        wfDebug( "TicketAuth: Creating new user account.\n" );
+        $userInfo = array();
+        if ( $realName != '' ) {
+            $userInfo['real_name'] = $realName;
+        }
+        if ( $email != '' ) {
+            $userInfo['email'] = $email;
+            $userInfo['email_authenticated'] = wfTimestampNow();
+        }
+        if ( $password != '' ) {
+            $userInfo['password'] = $password;    // incompatible with $wgPasswordSalt
+        }
+        $user = User::createNew( $username, $userInfo );
+        if ( !$user ) {
+            $error = 'TicketAuth: Could not create user.';
+            wfDebug( $error . "\n" );
+            return false;
+        }
+        $user->setOption( 'TicketAuth', true );
+        $user->saveSettings();
+        if ( session_id() == '' ) {
+            wfSetupSession();
+        }
+        $user->setCookies();
+        $wgUser = $user;
+        $wgOut->getContext()->setUser( $user );
+        wfRunHooks( 'AddNewAccount', array( $user, false ) );
+        $user->addNewUserLogEntry( 'autocreate', 'TicketAuth' );
+    } else {
+        wfDebug( "TicketAuth: Logging user in.\n" );
+        if ( !$user->getBoolOption( 'TicketAuth' ) ) {
+            $error = 'TicketAuth: User\'s account with this login name already exists and ' .
+                'it was not created by TicketAuth. Can\'t authenticate.';
+            wfDebug( $error . "\n" );
+            return false;
+        }
+        if ( $realName != '' ) {
+            $user->setRealName( $realName );
+        }
+        if ( $email != '' ) {
+            $user->setEmail( $email );
+            $user->mEmailAuthenticated = wfTimestampNow();
+        }
+        if ( $password != '' ) {
+            $user->mPassword = $password;    // incompatible with $wgPasswordSalt
+        }
+        $user->saveSettings();
+        if ( session_id() == '' ) {
+            wfSetupSession();
+        }
+        $user->setCookies();
+        $wgUser = $user;
+        $wgOut->getContext()->setUser( $user );
+    }
 
-	$result = true;
-	return true;
+    header('Location: '.explode('?',$wgRequest->getRequestURL())[0]);
+
+    $result = true;
+    return true;
 }
 
 function efTktAuth_OnPrefsPasswordAudit ( $user, $newPass, $error )
-	// $user: User (object) changing his password
-	// $newPass: new password
-	// $error: error (string) 'badretype', 'wrongpassword', 'error' or 'success'
+    // $user: User (object) changing his password
+    // $newPass: new password
+    // $error: error (string) 'badretype', 'wrongpassword', 'error' or 'success'
 {
-	if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
-		$error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
-		wfDebug( $error . "\n" );
-		die( $error );
-	}
+    if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
+        $error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
+        wfDebug( $error . "\n" );
+        die( $error );
+    }
 
-	if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
-		return true;
+    if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
+        return true;
 
-	if ( !$user->getBoolOption( 'TicketAuth' ) )
-		return true;
+    if ( !$user->getBoolOption( 'TicketAuth' ) )
+        return true;
 
-	throw new PasswordError( wfMsg( 'resetpass_forbidden' ) .
-		' <Extension:Ticket Authentication>' );
+    throw new PasswordError( wfMsg( 'resetpass_forbidden' ) .
+        ' <Extension:Ticket Authentication>' );
 
-	return false;
+    return false;
 }
 
 function efTktAuth_OnSpecialPasswordResetOnSubmit ( $users, $data, &$error )
-	// Hook available from version 1.18.0
-	// $users: array of User objects
-	// $data: array of data submitted by the user
-	// &$error: string, error code (message name) used to describe to error (out parameter).
-	//     The hook needs to return false when setting this, otherwise it will have no effect.
+    // Hook available from version 1.18.0
+    // $users: array of User objects
+    // $data: array of data submitted by the user
+    // &$error: string, error code (message name) used to describe to error (out parameter).
+    //     The hook needs to return false when setting this, otherwise it will have no effect.
 {
-	if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
-		$error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
-		wfDebug( $error . "\n" );
-		die( $error );
-	}
+    if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
+        $error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
+        wfDebug( $error . "\n" );
+        die( $error );
+    }
 
-	if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
-		return true;
+    if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
+        return true;
 
-	foreach ( $users as $user ) {
-		if ( $user->getBoolOption( 'TicketAuth' ) ) {
-			$error = wfMsg( 'resetpass_forbidden' ) .
-				' <Extension:Ticket Authentication>';
-			return false;
-		}
-	}
+    foreach ( $users as $user ) {
+        if ( $user->getBoolOption( 'TicketAuth' ) ) {
+            $error = wfMsg( 'resetpass_forbidden' ) .
+                ' <Extension:Ticket Authentication>';
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
 
 function efTktAuth_OnUserLoginMailPassword ( $username, &$error )
-	// Hook removed in version 1.18.0
-	// $name: the username to email the password of
-	// &$error: out-param - the error message to return
+    // Hook removed in version 1.18.0
+    // $name: the username to email the password of
+    // &$error: out-param - the error message to return
 {
-	if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
-		$error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
-		wfDebug( $error . "\n" );
-		die( $error );
-	}
+    if ( !isset( $GLOBALS['wgTktAuth_AllowPasswordChange'] ) ) {
+        $error = 'TicketAuth: wgTktAuth_AllowPasswordChange required global variable is not set.';
+        wfDebug( $error . "\n" );
+        die( $error );
+    }
 
-	if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
-		return true;
+    if ( $GLOBALS['wgTktAuth_AllowPasswordChange'] === true )
+        return true;
 
-	$user = User::newFromName( $username );
-	if ( !$user->getBoolOption( 'TicketAuth' ) )
-		return true;
+    $user = User::newFromName( $username );
+    if ( !$user->getBoolOption( 'TicketAuth' ) )
+        return true;
 
-	$error = wfMsg( 'resetpass_forbidden' ) .
-		' <Extension:Ticket Authentication>';
+    $error = wfMsg( 'resetpass_forbidden' ) .
+        ' <Extension:Ticket Authentication>';
 
-	return false;
+    return false;
 }
 
 function efTktAuth_RemoveSpecialPages(&$list)
