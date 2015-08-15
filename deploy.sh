53dc0faa027f355e8a8c3fd7243cb7a01c66c25b
Lars Vierbergen <vierbergenlars@gmail.com>
2015-08-15 22:49:58 +0200

    Add rollbarr error & deploy tracking

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,15 @@ php app/console assets:install --env=prod
 php app/console assetic:dump --env=prod
 php app/console braincrafted:bootstrap:install --env=prod
 php app/console doctrine:migrations:migrate --env=prod
+
+# Notify rollbar of deploy
+ACCESS_TOKEN=$(sed /rollbar/\!d < app/config/parameters.yml | cut -d: -f2 | tr -d " ")
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git log -n 1 --pretty=format:"%H"`
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
